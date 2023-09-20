86687fb0424a7ac693b35bf7bfa7c47ab92feaf0
codebjjung <jbj970324@gmail.com>
2023-09-20 16:22:22 +0900

    multiplier generator

--- multiplier/mul_gen.sv
+++ multiplier/mul_gen.sv
@@ -1,17 +1,17 @@
 class generator;
-	event drv_done;
 	mailbox drv_mbx;
+	virtual clk_if g_clk_if;
 
 	task run();
-		for (i=1; i < 10; i++) begin
-			Packet pkt = new;
+		for (int i=1; i<=10; i++) begin
+			Packet pkt = new();
+			@ (negedge g_clk_if.tb_clk);
 			pkt.randomize();
-			$display("[%0t] [Generator] %0d/10 create item", $time, i);
+			$display("[%0t] [Generator] Creating pkt %0d/10", $time, i);
 			drv_mbx.get(pkt);
-			$display("[%0t] [Generator] Generating complete", $time);
-			@(drv_done)
+			$display("[%0t] [Generator] Finish generating", $time);
 		end
 	endtask
 endclass
-		
-		
+
+
