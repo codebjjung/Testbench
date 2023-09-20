996bcc9ff0356a4cbf08e469b386f596a5d8b32f
codebjjung <jbj970324@gmail.com>
2023-09-20 17:22:06 +0900

    multiplier top on rtl

--- multiplier/mul_tb.sv
+++ multiplier/mul_tb.sv
@@ -1,5 +1,5 @@
 `timescale 1ns/1ps
-`include "design.sv"
+`include "rtl.sv"
 `include "mul_if.sv"
 `include "mul_tr.sv"
 `include "mul_gen.sv"
@@ -11,17 +11,23 @@
 
 module tb;
 	bit tb_clk;
+	logic [3:0] a,b;
+	logic [7:0] y;
 
-	clk_if my_clk_if();
-	mul_if my_if;
-	mul dut(my_if);
+	clk_if m_clk_if();
+	mul_if m_if;
+	mul dut(a,b,y);
+
+	assign a = m_if.a;
+	assign b = m_if.b;
+	assign m_if.y = y;
 
 	initial begin
-		teset t0;
+		test t0;
 
 		t0 = new;
-		t0.e0.e_vif = my_if;
-		t0.e0.e_clk_vif = my_clk_if;
+		t0.e0.e_vif = m_if;
+		t0.e0.e_clk_vif = m_clk_if;
 		t0.run();
 
 		#50 $finish;
