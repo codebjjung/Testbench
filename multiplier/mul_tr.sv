0c53e3265df7bf2fc1324cad32aa31e91488a0f1
codebjjung <jbj970324@gmail.com>
2023-09-20 16:19:35 +0900

    multiplier transaction object

--- multiplier/mul_tr.sv
+++ multiplier/mul_tr.sv
@@ -2,4 +2,14 @@ class Packet;
 	rand bit [3:0] a;
 	rand bit [3:0] b;
 	bit [7:0] y;
+
+	function new(string tag="");
+		$display ("[%0t] [%s] a=%0d b=%0d y=%0d", $time, tag, a, b, y);
+	endfunction
+
+	function void copy(Packet tmp);
+		this.a = tmp.a;
+		this.b = tmp.b;
+		this.y = tmp.y;
+	endfunction
 endclass
