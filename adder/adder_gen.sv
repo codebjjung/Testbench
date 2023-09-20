5bbf40570725050f173c6a30d55328c460a3de9a
codebjjung <jbj970324@gmail.com>
2023-09-20 13:24:31 +0900

    adder generator

--- adder/adder_gen.sv
+++ adder/adder_gen.sv
@@ -6,7 +6,7 @@ class generator;
 		for (int i = 0; i < 10; i++) begin
 			Packet pkt = new;
 			pkt.randomize();
-			$display ("T=%0t [Generator] Loop:%0d/10 create next item", $time, i+1);
+			$display ("T=%0t [Generator]  creating pkt %0d/10", $time, i+1);
 			drv_mbx.put(item);
 			$display ("T=%0t [Generator] Wait driver", $time);
 			@(drv_done);
