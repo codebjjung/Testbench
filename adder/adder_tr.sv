3112ee9400a75ed1aeeb92fcba7d0c9b525d1a87
codebjjung <jbj970324@gmail.com>
2023-09-20 13:29:26 +0900

    adder transaction object

--- adder/adder_tr.sv
+++ adder/adder_tr.sv
@@ -3,7 +3,6 @@ class Packet;
 	rand bit[7:0] a;
 	rand bit[7:0] b;
 	bit[7:0]		sum;
-	bit			carry;
 
 	function void print(string tag="");
 		$display ("T=%0t %s a=0x%0h b=0x%0h sum=0x%0h carry=0x%0h", $time, tag, a, b, sum, carry);
