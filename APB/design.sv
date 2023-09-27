bc1ad433cb845a227599d7fc0a1e274a195308d2
codebjjung <jbj970324@gmail.com>
2023-09-27 10:47:39 +0900

    APB design v1

--- APB/design.sv
+++ APB/design.sv
@@ -1,2 +1,19 @@
-module APB(
+module APB_SLAVE
+(
+	input clk,
+	input n_rst,
+	input [ADDR_WIDTH-1:0] paddr,
+	input [2:0] prot,
+	input pwrite,
+	input psel,
+	input penable,
+	input [DATA_WIDTH-1:0] pwdata,
+	input [DATA_STRB-1:0] pstrb,
+	output pready,
+	output reg slverr,
+	output reg [DATA_WIDTH-1:0] prdata	
 );
+
+reg [1:0] IDLE = 2'b00;
+reg [1:0] SETUP = 2'b01;
+reg [1:0] ACCESS = 2'b10;
