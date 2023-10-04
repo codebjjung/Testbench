b64a85d490cb5b46a875445034bd9d830715111d
codebjjung <jbj970324@gmail.com>
2023-10-04 11:21:41 +0900

    apb design

--- APB/design.sv
+++ APB/design.sv
@@ -5,9 +5,12 @@
 `define AXI4_MO 16
 
 module APB_SLAVE
+	#(ADDR_WIDTH = 32,
+	DATA_WIDTH = 32,
+	DATA_STRB = (DATA_WIDTH/8))
 (
 	input clk,
-	input n_rst,
+	input nrst,
 	input [ADDR_WIDTH-1:0] paddr,
 	input [2:0] prot,
 	input pwrite,
@@ -16,18 +19,19 @@ module APB_SLAVE
 	input [DATA_WIDTH-1:0] pwdata,
 	input [DATA_STRB-1:0] pstrb,
 	output pready,
-	output reg slverr,
+	output reg slverr=0, //temp
 	output reg [DATA_WIDTH-1:0] prdata	
 );
 
-reg [1:0] IDLE = 2'b00;
-reg [1:0] SETUP = 2'b01;
-reg [1:0] ACCESS = 2'b10;
+parameter IDLE = 2'b00;
+parameter SETUP = 2'b01;
+parameter ACCESS = 2'b10;
 
+//MEM SAVE
 int mem[`MEM_SIZE];
 
 wire pready;
-reg [1:0] st,n_rst;
+reg [1:0] st,nst;
 wire trans;
 wire [DATA_WIDTH-1:0] s_pwdata;
 
@@ -35,13 +39,13 @@ assign pready = psel;
 assign trans = psel;
 assign s_pwdata = prot[1] ? 32'h0 : pwdata & {{8{pstrb[3]}}, {8{pstrb[2]}}, {8{pstrb[1]}}, {8{pstrb[0]}}};
 
-always @(posedge clk or negedge n_rst) begin
-	if(!n_rst) begin
+always @(posedge clk or negedge nst) begin
+	if(!nst) begin
 		prdata <= 0;
 		st <= 0;
 		nst <= 0;
 	end else begin
-		st <= n_rst;
+		st <= nst;
 	end
 
 	if(st == ACCESS) begin
@@ -55,13 +59,14 @@ always @(posedge clk or negedge n_rst) begin
 	end
 end
 
-always @(posedge clk or negedge n_rst) begin
+//APB FSM
+always @(posedge clk or negedge nst) begin
 	case(st)
 		IDLE : begin
 			if(psel)
-				n_rst <= SETUP;
+				nst <= SETUP;
 			else
-				n_rst <= IDLE;
+				nst <= IDLE;
 		end
 		SETUP : begin
 				nst <= ACCESS;
