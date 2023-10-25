ab45a53c24e5e4fe42bcbd142ed8d8dbe1145afc
codebjjung <jbj970324@gmail.com>
2023-10-25 19:26:21 +0900

    apb generator

--- APB/apb_gen.sv
+++ APB/apb_gen.sv
@@ -3,9 +3,14 @@ class generator;
    mailbox drv_mbx;
 
    task run();
-      for (int i = 0; i < 10; i++) begin
+      for (int i = 0; i < 20; i++) begin
          Packet pc = new;
          pc.randomize();
+         if (i <= 9) begin
+            pc.paddr = i;
+         end else begin
+            pc.paddr = i - 10;
+         end
          drv_mbx.put(pc);
          $display("[%0t] [Generator] [%0d/10] Randomizing..", $time, i);
       end
