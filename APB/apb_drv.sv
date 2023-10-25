ffc2235619b74af9065f0be7aa2615a608c594e4
codebjjung <jbj970324@gmail.com>
2023-10-25 19:29:37 +0900

    apb driver

--- APB/apb_drv.sv
+++ APB/apb_drv.sv
@@ -6,18 +6,20 @@ class driver;
 
    task run();
       $display("[%0t] [Driver] Driver starting..", $time);
-      forever begin
+      boot();
+      repeat (10) begin
          Packet pc;
-         $display("[%0t] [Driver] Waiting item", $time);
          drv_mbx.get(pc);
-         boot();
          apb_write(pc.paddr, pc.pwdata);
+      end
+      repeat (10) begin
+         Packet pc;
+         drv_mbx.get(pc);
          apb_read(pc.paddr);
       end
    endtask
 
    task boot;
-      clk_vif.clk = 0;
       d_vif.nrst = 0;
       d_vif.paddr = 0;
       d_vif.pstrb = 4'b1111;
@@ -29,7 +31,7 @@ class driver;
    endtask
 
    task apb_write(
-         input bit [7:0] addr,
+         input bit [3:0] addr,
          input bit [31:0] data
    );
 
@@ -45,7 +47,7 @@ class driver;
    @ (negedge clk_vif.clk);
       d_vif.psel = 0;
       d_vif.penable = 0;
-      $display("[%0t] Write finish, pwdata = %0h", d_vif.pwdata);
+      $display("[%0t] WRITE FINISH!! paddr : %0h, pwdata : %0h", d_vif.paddr, d_vif.pwdata);
    endtask
 
    task apb_read(input bit [7:0] addr);
