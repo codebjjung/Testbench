90e0e59b421222f0aa8f0bb37c63a0e8125d60f9
codebjjung <jbj970324@gmail.com>
2023-10-25 15:18:33 +0900

    APB design

--- APB/design.sv
+++ APB/design.sv
@@ -31,57 +31,46 @@ parameter ACCESS = 2'b10;
 int mem[`MEM_SIZE];
 
 wire pready;
-reg [1:0] st,nst;
-wire trans;
+reg [1:0] st;
 wire [DATA_WIDTH-1:0] s_pwdata;
 
 assign pready = psel;
-assign trans = psel;
 assign s_pwdata = prot[1] ? 32'h0 : pwdata & {{8{pstrb[3]}}, {8{pstrb[2]}}, {8{pstrb[1]}}, {8{pstrb[0]}}};
 
-always @(posedge clk or negedge nst) begin
-	if(!nst) begin
+always @(posedge clk or negedge nrst) begin
+	if(!nrst) begin
 		prdata <= 0;
 		st <= 0;
-		nst <= 0;
-	end else begin
-		st <= nst;
 	end
 
-	if(st == ACCESS) begin
-		if(psel & penable & pready) begin
-			if(pwrite) begin
-				mem[paddr - `BASE_ADDR] <= s_pwdata;
-			end else begin
-				prdata <= mem[paddr - `BASE_ADDR];
-			end
-		end
-	end
+   if(psel & penable & pready) begin
+      if(pwrite) begin
+         mem[paddr] <= s_pwdata;
+      end else begin
+         prdata <= mem[paddr];
+      end
+   end
 end
 
 //APB FSM
-always @(posedge clk or negedge nst) begin
+always @(posedge clk or negedge nrst) begin
 	case(st)
 		IDLE : begin
-			if(psel)
-				nst <= SETUP;
+			if(psel & !penable)
+				st <= SETUP;
 			else
-				nst <= IDLE;
+				st <= IDLE;
 		end
 		SETUP : begin
-				nst <= ACCESS;
+         if(psel & penable)
+				st <= ACCESS;
 		end
-		ACCESS : begin
-			if(psel & penable) begin
-				if(pready)
-					if(trans)
-						nst <= SETUP;
-					else
-						nst <= IDLE;
-				else
-					nst <= ACCESS;
-			end
-		end
+      ACCESS : begin
+         if(pready)
+            st <= IDLE;
+         else
+            st <= ACCESS;
+      end
 	endcase
 end
 endmodule
