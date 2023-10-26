d8fd8471c4979de6a41e1817428ffb6d9a08f72d
codebjjung <jbj970324@gmail.com>
2023-10-26 10:43:01 +0900

    apb monitor

--- APB/apb_mon.sv
+++ APB/apb_mon.sv
@@ -6,12 +6,21 @@ class monitor;
    task run();
       $display("[%0t] [Monitor] Monitor starting..", $time);
 
-      for (int i=0; i<10; i++) begin
+      for (int i=0; i<20; i++) begin
          Packet pc = new();
+         if (i == 0) begin
+            #40;
+         end else begin
+            #30;
+         end
          pc.paddr = m_vif.paddr;
          pc.pwdata = m_vif.pwdata;
          pc.prdata = m_vif.prdata;
          scb_mbx.put(pc);
+         if (pc.prdata)
+            $display("[%0t] [Monitor] paddr : %0h prdata : %0h", $time, pc.paddr, pc.prdata);
+         else
+            $display("[%0t] [Monitor] paddr : %0h pwdata : %0h", $time, pc.paddr, pc.pwdata);
       end
    endtask
 endclass
