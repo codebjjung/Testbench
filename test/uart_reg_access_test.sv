1f97dc683e70e19fd854237539b42dd63bf344d7
codebjjung <jbj970324@gmail.com>
2023-12-06 14:31:48 +0900

    reset, write test

--- test/uart_reg_access_test.sv
+++ test/uart_reg_access_test.sv
@@ -18,3 +18,32 @@
    bit [31:0] wdata, rdata;
 
    int i, err_count;
+
+   //reset value test
+   task rst_test(input int i);
+      ca53_read(`UART0_BASE + offset_q[i], rdata);
+      if((rdata & mask_q[i]) == (reset_value_q[i] & mask_q[i])) begin
+         $display("pass, rdata = %0h reset_value = %0h", rdata, reset_value_q[i]);
+      end else begin
+         $display("fail, rdata = %0h reset_value = %0h", rdata, reset_value_q[i]);
+         err_count++;
+      end
+   endtask
+
+   //write 0 test
+   task write_0_test(input int i);
+      ca53_write(`UART0_BASE + offset_q[i], wdata);
+      ca53_read(`UART0_BASE + offset_q[i], rdata);
+      if((rdata & mask_q[i]) == (wdata & mask_q[i])) begin
+         $display("pass");
+      end else begin
+         if(reg_type[i] == "R") begin
+            $display("waive");
+         end else begin
+            $display("fail");
+            err_count++;
+         end
+      end
+   endtask
+
+
